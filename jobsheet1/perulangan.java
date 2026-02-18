52b0d48010d33321721b11036c7ec6609e11fc4e
Haidhar Annabigh <haidharannabighhh@email.com>
2026-02-18 21:50:19 +0700

    perulangan jobsheet 1

---
+++ jobsheet1/perulangan.java
@@ -0,0 +1,30 @@
+package jobsheet1;
+import java.util.Scanner;
+
+public class perulangan {
+    public static void main(String[] args) {
+        Scanner haidhar = new Scanner(System.in);
+        
+        System.out.print("Masukkan NIM: ");
+        String nim = haidhar.nextLine();
+
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+        if (n < 10) {
+            n= n + 10;
+        }
+        System.out.println("n = " + n);
+        System.out.print("Output: ");
+        for (int i = 1; i <= n; i++) {
+            if (i == 10 | i == 15) {
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.print("# ");
+            } else if (i % 2 == 0) {
+                System.out.print(i + " ");
+            } else {
+                System.out.print("* ");
+            }
+        }
+    }
+}
