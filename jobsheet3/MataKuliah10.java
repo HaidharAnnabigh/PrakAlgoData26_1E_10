c74d0adc5fd51e418ea4ab3372174848ee7dc271
Haidhar Annabigh <haidharannabighhh@email.com>
2026-02-26 09:20:02 +0700

    percobaan3

---
+++ jobsheet3/MataKuliah10.java
@@ -0,0 +1,15 @@
+package jobsheet3;
+
+public class MataKuliah10 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jmlJam;
+
+    public MataKuliah10(String kode, String nama, int sks, int jmlJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jmlJam = jmlJam;
+    }
+}
---
+++ jobsheet3/MatakuliahDemo.java
@@ -0,0 +1,39 @@
+package jobsheet3;
+import java.util.Scanner;
+public class MatakuliahDemo {
+    public static void main(String[] args) {
+        Scanner haidhar = new Scanner(System.in);
+        MataKuliah10[] arrayMatkul = new MataKuliah10[3];
+        String kode, nama, dummy;
+        int sks, jmlJam;
+
+        for(int i=0; i < 3; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode      : ");
+            kode = haidhar.nextLine();
+            System.out.print("Nama      : ");
+            nama = haidhar.nextLine();
+            System.out.print("SKS       : ");
+            dummy = haidhar.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam: ");
+            dummy = haidhar.nextLine();
+            jmlJam = Integer.parseInt(dummy);
+            System.out.println("--------------------------------");
+
+            arrayMatkul[i] = new MataKuliah10(kode, nama, sks, jmlJam);
+
+            
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan data Mata Kuliah ke-" + (i+1));
+            System.out.println("Kode Mata Kuliah: " + arrayMatkul[i].kode);
+            System.out.println("Nama Mata Kuliah: " + arrayMatkul[i].nama);
+            System.out.println("Jumlah SKS      : " + arrayMatkul[i].sks);
+            System.out.println("Jumlah Jam      : " + arrayMatkul[i].jmlJam);
+            System.out.println("----------------------------------------------");
+            
+        }
+    }
+}
