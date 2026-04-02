db13c20ee41125ba3bc5f46e8e425bfd402ec0e6
Haidhar Annabigh <haidharannabighhh@email.com>
2026-04-02 09:34:14 +0700

    praktikum1

---
+++ Praktikum05/SortingMain10.java
@@ -0,0 +1,34 @@
+package Praktikum05;
+
+public class SortingMain10 {
+
+    public static void main(String[] args) {
+        
+        int a[] = {20, 10, 2, 7, 12};
+        Sorting10 dataUrut1 = new Sorting10(a, a.length);
+        
+        System.out.println("Data awal 1");
+        dataUrut1.tampil();
+        dataUrut1.bubbleSort();
+        System.out.println("Data yang sudah diurutkan dengan Bubble Sort (ASC)");
+        dataUrut1.tampil();
+
+        int b[] = {30, 20, 2, 8, 14};
+        Sorting10 dataUrut2 = new Sorting10(b, b.length);
+
+        System.out.println("Data awal 2");
+        dataUrut2.tampil();
+        dataUrut2.bubbleSort();
+        System.out.println("Data yang sudah diurutkan dengan Bubble Sort (ASC)");
+        dataUrut2.tampil();
+
+        int c[] = {40, 10, 4, 9, 3};
+        Sorting10 dataUrut3 = new Sorting10(c, c.length);
+
+        System.out.println("Data awal 3");
+        dataUrut3.tampil();
+        dataUrut3.bubbleSort();
+        System.out.println("Data yang sudah diurutkan dengan Bubble Sort (ASC)");
+        dataUrut3.tampil();
+    }
+}
