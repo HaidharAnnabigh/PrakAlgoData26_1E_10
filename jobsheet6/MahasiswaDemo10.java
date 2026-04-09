85252cfd975ecfb37c0028ff4a8bef42bcd96a40
Haidhar Annabigh <haidharannabighhh@email.com>
2026-04-09 11:55:50 +0700

    percobaan1

---
+++ jobsheet6/MahasiswaDemo10.java
@@ -0,0 +1,67 @@
+package jobsheet6;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo10 {
+
+    public static void main(String[] args) {
+        Scanner haidhar = new Scanner(System.in);
+
+        MahasiswaBerprestasi10 list = new MahasiswaBerprestasi10();
+        int jumMhs = 5;
+
+        for (int i = 1; i <= jumMhs; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + i);
+            System.out.print("Nama  : ");
+            String nama = haidhar.nextLine();
+            System.out.print("NIM   : ");
+            String nim = haidhar.nextLine();
+            System.out.print("IPK   : ");
+            double ipk = haidhar.nextDouble();
+            haidhar.nextLine();
+            System.out.print("Kelas : ");
+            String kelas = haidhar.nextLine();
+            System.out.println("------------------------------");
+
+            list.tambah(new Mahasiswa10(nim, nama, kelas, ipk));
+        }
+
+        list.tampil();
+        
+        System.out.println("--------------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("--------------------------------------------------");
+        System.out.println("masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = haidhar.nextDouble();
+
+        System.out.println("menggunakan sequential searching");
+        int pss = list.sequentialSearching(cari);
+
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
+
+        // System.out.println("\nData mahasiswa sebelum sorting: ");
+        // list.tampil();
+
+        // System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) :
+        // ");
+        // list.bubbleSort();
+        // list.tampil();
+
+        // System.out.println("Data yang sudah terurut menggunakan SELECTION SORT
+        // (ASC)");
+        // list.selectionSort();
+        // list.tampil();
+
+        // System.out.println("Data yang sudah terurut menggunakan INSERTION SORT
+        // (ASC)");
+        // list.insertionSort();
+        // list.tampil();
+
+        // System.out.println("Data yang sudah terurut menggunakan INSERTION SORT
+        // (DESC)");
+        // list.insertionSortDesc();
+        // list.tampil();
+    }
+}
