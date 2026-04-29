22f906d66892e1987637ba915ab06df5e0d29e72
Haidhar Annabigh <haidharannabighhh@email.com>
2026-04-29 22:21:51 +0700

    tugas

--- Jobsheet9/MainSurat.java
+++ Jobsheet9/MainSurat.java
@@ -1,7 +1,7 @@
 package Jobsheet9;
 
 import java.util.Scanner;
-
+ 
 public class MainSurat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
--- Jobsheet9/Surat10.java
+++ Jobsheet9/Surat10.java
@@ -9,12 +9,12 @@ public class Surat10 {
 
     public Surat10() {
     }
-
+ 
     public Surat10(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
         this.idSurat = idSurat;
         this.namaMahasiswa = namaMahasiswa;
         this.kelas = kelas;
         this.jenisIzin = jenisIzin;
-        this.durasi = durasi;
+        this.durasi = durasi; 
     }
 }
