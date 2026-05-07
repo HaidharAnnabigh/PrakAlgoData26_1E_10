63dcae8c39022ce10dfc685266dc23fb8a9d4af8
Haidhar Annabigh <haidharannabighhh@email.com>
2026-05-07 08:47:54 +0700

    modifikasi1

--- Pertemuan12/SLLMain10.java
+++ Pertemuan12/SLLMain10.java
@@ -7,22 +7,36 @@ public class SLLMain10 {
         Scanner sc = new Scanner(System.in);
         SingleLinkedList10 sll = new SingleLinkedList10();
 
-        Mahasiswa10 mhs1 = new Mahasiswa10("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa10 mhs2 = new Mahasiswa10("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa10 mhs3 = new Mahasiswa10("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa10 mhs4 = new Mahasiswa10("21212203", "Dirga", "4D", 3.6);
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
 
-        sll.print();
-        sll.addFirst(mhs4);
+        for (int i = 0; i < jumlah; i++) {
+
+            System.out.println("\nData Mahasiswa ke-" + (i + 1));
+
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+
+            Mahasiswa10 mhs = new Mahasiswa10(nim, nama, kelas, ipk);
+
+            sll.addLast(mhs);
+            System.out.println();
+        }
+
+        System.out.println("Data Linked List Mahasiswa");
         sll.print();
 
-        sll.addLast(mhs1);
-        sll.print();
-
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
-        
         sc.close();
     }
 }
