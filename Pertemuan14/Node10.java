36a3907753eaeda8074407aa008eb189640a4ab1
Haidhar Annabigh <haidharannabighhh@email.com>
2026-06-03 16:09:50 +0700

    percobaan1

---
+++ Pertemuan14/Node10.java
@@ -0,0 +1,15 @@
+package Pertemuan14;
+
+public class Node10 {
+
+    Mahasiswa10 mahasiswa;
+    Node10 right, left;
+
+    public Node10() {
+    }
+
+    public Node10(Mahasiswa10 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
